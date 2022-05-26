d74c42cd166767b8bb54d65b2eaaf40cb5e6f8c7
JazzyLucas <lucasrendell@rocketmail.com>
2022-05-26 02:05:14 -0400

    Level Generation + Navigation (AI)
    Scene: Assets\_Test\Lucas\Scenes\LucasDungenTest
    + Added the AI Navigation package.
    + New map generation bakes a navmesh along with it.
    + New organization for floor, walls, entrances, and hooks for various events/pickupables.
    
    Soon the sky will be the limit on this.

---
+++ Assets/_Test/Lucas/Scripts/SilverlyBeeDungeonGenerator/SBDungeonGenerator.cs
@@ -0,0 +1,250 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.AI.Navigation;
+using Unity.AI;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SBDungeonGenerator : MonoBehaviour
+{
+    public class Cell
+    {
+        public bool visited = false;
+        public bool[] status = new bool[4];
+    }
+
+    [System.Serializable]
+    public class Rule
+    {
+        public GameObject room;
+        public Vector2Int minPosition;
+        public Vector2Int maxPosition;
+
+        public bool obligatory;
+
+        public int ProbabilityOfSpawning(int x, int y)
+        {
+            // 0 - cannot spawn 1 - can spawn 2 - HAS to spawn
+
+            if (x>= minPosition.x && x<=maxPosition.x && y >= minPosition.y && y <= maxPosition.y)
+            {
+                return obligatory ? 2 : 1;
+            }
+
+            return 0;
+        }
+
+    }
+
+    public Vector2Int size;
+    public int startPos = 0;
+    public Rule[] rooms;
+    public Vector2 offset;
+
+    private List<Cell> board;
+    private List<GameObject> objectPool;
+
+    void Start()
+    {
+        DoMazeGenerator();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            DoMazeGenerator();
+        }
+    }
+
+    void GenerateDungeon()
+    {
+
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
+                Cell currentCell = board[(i + j * size.x)];
+                if (currentCell.visited)
+                {
+                    int randomRoom = -1;
+                    List<int> availableRooms = new List<int>();
+
+                    for (int k = 0; k < rooms.Length; k++)
+                    {
+                        int p = rooms[k].ProbabilityOfSpawning(i, j);
+
+                        if(p == 2)
+                        {
+                            randomRoom = k;
+                            break;
+                        } else if (p == 1)
+                        {
+                            availableRooms.Add(k);
+                        }
+                    }
+
+                    if(randomRoom == -1)
+                    {
+                        if (availableRooms.Count > 0)
+                        {
+                            randomRoom = availableRooms[Random.Range(0, availableRooms.Count)];
+                        }
+                        else
+                        {
+                            randomRoom = 0;
+                        }
+                    }
+
+
+                    var newRoom = Instantiate(rooms[randomRoom].room, new Vector3(i * offset.x, 0, -j * offset.y), Quaternion.identity, transform).GetComponent<SBRoomBehaviour>();
+                    newRoom.UpdateRoom(currentCell.status);
+                    newRoom.name += " " + i + "-" + j;
+                    objectPool.Add(newRoom.gameObject);
+                }
+            }
+        }
+
+    }
+
+    void DoMazeGenerator()
+    {
+        // Initializations
+        if (objectPool != null)
+        {
+            foreach (var obj in objectPool)
+            {
+                Destroy(obj);
+            }
+            objectPool.Clear();
+        }
+        objectPool ??= new List<GameObject>();
+        board = new List<Cell>();
+
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
+                board.Add(new Cell());
+            }
+        }
+
+        int currentCell = startPos;
+
+        Stack<int> path = new Stack<int>();
+
+        // Limit
+        int k = 0;
+
+        while (k<1000)
+        {
+            k++;
+
+            board[currentCell].visited = true;
+
+            if(currentCell == board.Count - 1)
+            {
+                break;
+            }
+
+            //Check the cell's neighbors
+            List<int> neighbors = CheckNeighbors(currentCell);
+
+            if (neighbors.Count == 0)
+            {
+                if (path.Count == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    currentCell = path.Pop();
+                }
+            }
+            else
+            {
+                path.Push(currentCell);
+
+                int newCell = neighbors[Random.Range(0, neighbors.Count)];
+
+                if (newCell > currentCell)
+                {
+                    //down or right
+                    if (newCell - 1 == currentCell)
+                    {
+                        board[currentCell].status[2] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[3] = true;
+                    }
+                    else
+                    {
+                        board[currentCell].status[1] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[0] = true;
+                    }
+                }
+                else
+                {
+                    //up or left
+                    if (newCell + 1 == currentCell)
+                    {
+                        board[currentCell].status[3] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[2] = true;
+                    }
+                    else
+                    {
+                        board[currentCell].status[0] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[1] = true;
+                    }
+                }
+
+            }
+
+        }
+        GenerateDungeon();
+        InitializeNavMeshes();
+    }
+
+    private void InitializeNavMeshes()
+    {
+        var surfaces =  FindObjectsOfType<NavMeshSurface>();
+        foreach (var s in surfaces)
+        {
+            s.BuildNavMesh();
+        }
+    }
+
+    private List<int> CheckNeighbors(int cell)
+    {
+        List<int> neighbors = new List<int>();
+
+        //check up neighbor
+        if (cell - size.x >= 0 && !board[(cell-size.x)].visited)
+        {
+            neighbors.Add((cell - size.x));
+        }
+
+        //check down neighbor
+        if (cell + size.x < board.Count && !board[(cell + size.x)].visited)
+        {
+            neighbors.Add((cell + size.x));
+        }
+
+        //check right neighbor
+        if ((cell+1) % size.x != 0 && !board[(cell +1)].visited)
+        {
+            neighbors.Add((cell +1));
+        }
+
+        //check left neighbor
+        if (cell % size.x != 0 && !board[(cell - 1)].visited)
+        {
+            neighbors.Add((cell -1));
+        }
+
+        return neighbors;
+    }
+}
